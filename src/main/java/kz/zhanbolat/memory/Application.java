edf27de60c416b2207c2e6d5e6d8123358d36a63
Zhanbolat Orakbayev <fous363@gmail.com>
2020-10-17 11:52:02 +0600

    Complete exercise 1

--- src/main/java/kz/zhanbolat/memory/Application.java
+++ src/main/java/kz/zhanbolat/memory/Application.java
@@ -3,6 +3,8 @@ package kz.zhanbolat.memory;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        /* Uncomment to throw java.lang.OutOfMemoryError: Requested array size exceeds VM limit
+        int size = Integer.MAX_VALUE;
+        long[] memory = new long[size];*/
     }
 }
