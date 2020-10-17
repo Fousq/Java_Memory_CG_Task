95ce5e198e9bd16345bf1049cba3814a10766792
Zhanbolat Orakbayev <fous363@gmail.com>
2020-10-17 14:28:09 +0600

    Refactor

--- src/main/java/kz/zhanbolat/memory/Application.java
+++ src/main/java/kz/zhanbolat/memory/Application.java
@@ -1,12 +1,9 @@
 package kz.zhanbolat.memory;
 
-import javassist.CannotCompileException;
-import javassist.ClassPool;
-
 public class Application {
-    private static ClassPool classPool = ClassPool.getDefault();
 
-    public static void main(String[] args) throws CannotCompileException {
+
+    public static void main(String[] args) throws javassist.CannotCompileException {
         /* Uncomment to throw java.lang.OutOfMemoryError: Java heap space
         java.util.List<Integer> list = new java.util.ArrayList<>();
 
@@ -16,16 +13,16 @@ public class Application {
             list.add(random.nextInt());
         }*/
         /* Uncomment to throw java.lang.OutOfMemoryError: Java heap space.
-            Warning: can take a lot of time to cause the error
+            Warning: can take a lot of time to cause the error if -Xmx is not changed
         java.util.Random random = new java.util.Random();
         ContinuousObject continuousObject = new ContinuousObject(random.nextInt(), random.nextInt(), random.nextInt());
-        final long start = System.currentTimeMillis();
         while(true) {
             continuousObject = new ContinuousObject(random.nextInt(), random.nextInt(), random.nextInt(), continuousObject);
         }*/
         /* Uncomment to throw java.lang.OutOfMemoryError: java.lang.OutOfMemoryError: Metaspace.
             Warning: can take a lot of time to cause the error
             if the option -XX:MaxMetaspaceSize is not changed
+        javassist.ClassPool classPool = ClassPool.getDefault();
         for (long i = 0;;i++) {
             Class clazz = classPool.makeClass("kz.zhanbolat.memory.ContinuousObject" + i).toClass();
         }*/
